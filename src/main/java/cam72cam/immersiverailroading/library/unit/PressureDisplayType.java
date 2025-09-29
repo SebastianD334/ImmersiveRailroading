63b263c11d8312cfa399dbfa868aeca60e8c0861
SebastianD334 <sebastian.dunskus@icloud.com>
2025-09-29 16:17:12 +0200

    Added bar as definable max pressure unit and implemented new style for max_pressure definitions
    kept max_psi, but changed max_kpa to max_pressure_kpa, since I don't think anyone has used it yet, additionally, max_pressure_psi and max_pressure_bar also exist.

--- src/main/java/cam72cam/immersiverailroading/library/unit/PressureDisplayType.java
+++ src/main/java/cam72cam/immersiverailroading/library/unit/PressureDisplayType.java
@@ -7,6 +7,8 @@ public enum PressureDisplayType {
 
     public static final float psiToKPa = 6.89474f;
     public static final float kPaToPsi = 0.145037f;
+    public static final float psiToBar = 0.0689474f;
+    public static final float BarToPsi = 14.5037f;
 
     public float convertFromPSI(float value) {
         switch (this) {
--- src/main/java/cam72cam/immersiverailroading/registry/LocomotiveSteamDefinition.java
+++ src/main/java/cam72cam/immersiverailroading/registry/LocomotiveSteamDefinition.java
@@ -54,8 +54,12 @@ public class LocomotiveSteamDefinition extends LocomotiveDefinition {
             tankCapacity_l = properties.getValue("water_capacity_l").asInteger() * internal_inv_scale;
             if (properties.getValue("max_psi").asFloat() != null) {
                 maxPSI = properties.getValue("max_psi").asFloat() * internal_inv_scale;
+            } else if (properties.getValue("max_pressure_psi").asFloat() != null) {
+                maxPSI = properties.getValue("max_pressure_psi").asFloat() * internal_inv_scale;
+            } else if (properties.getValue("max_pressure_bar").asFloat() != null) {
+                maxPSI = properties.getValue("max_pressure_bar").asFloat() * PressureDisplayType.BarToPsi * internal_inv_scale;
             } else {
-                maxPSI = properties.getValue("max_kpa").asFloat() * PressureDisplayType.kPaToPsi * internal_inv_scale;
+                maxPSI = properties.getValue("max_pressure_kpa").asFloat() * PressureDisplayType.kPaToPsi * internal_inv_scale;
             }
             numSlots = Math.ceil(firebox.getValue("slots").asInteger() * internal_inv_scale);
             width = Math.ceil(firebox.getValue("width").asInteger() * internal_inv_scale);
